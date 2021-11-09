be15a323795e6b0ddb52a94b6ad0d00eaa2f0c38
Radonevsky <Radonevsky@users.noreply.github.com>
2021-11-10 02:58:13 +0300

    fix: code highlighter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import theme from './css/theme.css'
+
 import style from './css/main.scss'
 import header__photo from './img/profile-photo.jpg'
 import sample_flowers_page from './img/sample_flowers_page.png'
@@ -6,6 +6,10 @@ import sample_logistic_landing from './img/sample_logistic_landing.png'
 import Octocat from './img/Octocat.png'
 import GitHub_Logo from './img/GitHub_Logo.png'
 import rs_school_js from './img/rs_school_js.svg'
+import english from './img/english.svg'
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+import 'prismjs/components/prism-javascript.js'
 
-
+Prism.highlightAll();
 console.log('I am index.js!')
